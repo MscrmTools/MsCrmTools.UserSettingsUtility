ba4d42927307a3d740a8c84ad5b57fff5400ae70
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-03-21 16:42:32 +0100

    Initial commit

---
+++ MsCrmTools.UserSettingsUtility/AppCode/Currency.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace MsCrmTools.UserSettingsUtility.AppCode
+{
+    internal class Currency
+    {
+        private readonly Entity currency;
+
+        public Currency(Entity currency)
+        {
+            this.currency = currency;
+        }
+
+        public EntityReference CurrencyReference
+        {
+            get { return currency.ToEntityReference(); }
+        }
+
+        public override string ToString()
+        {
+            return currency.GetAttributeValue<string>("currencyname");
+        }
+    }
+}
---
+++ MsCrmTools.UserSettingsUtility/AppCode/TimeZoneInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace MsCrmTools.UserSettingsUtility.AppCode
+{
+    internal class TimeZone
+    {
+        public int Code { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
